1b88e8e0cc956572ecb10236905306ca8ab82c4c
5Daydreams <nelsonkossuga@gmail.com>
2022-01-04 20:23:25 +0100

    project broken by precompiled headers;

---
+++ Hazel/src/hzpch.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifdef HZ_PLATFORM_WINDOWS
+#ifndef NOMINMAX
+// See github.com/skypjack/entt/wiki/Frequently-Asked-Questions#warning-c4003-the-min-the-max-and-the-macro
+#define NOMINMAX
+#endif
+#endif // HZ_PLATFORM_WINDOWS
+
+
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#ifdef HZ_PLATFORM_WINDOWS
+	#include <Windows.h>
+#endif
