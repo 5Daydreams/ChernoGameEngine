3023a07a32f9f4070e7dc5c491c0f57679f12002
5Daydreams <nelsonkossuga@gmail.com>
2022-01-04 21:37:16 +0100

    fixed precompiled header issues, now working on fixing glfw;

--- Hazel/src/hzpch.h
+++ Hazel/src/hzpch.h
@@ -5,7 +5,7 @@
 // See github.com/skypjack/entt/wiki/Frequently-Asked-Questions#warning-c4003-the-min-the-max-and-the-macro
 #define NOMINMAX
 #endif
-#endif // HZ_PLATFORM_WINDOWS
+#endif
 
 
 #include <iostream>
@@ -19,6 +19,8 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+
+#include "Hazel/Log.h"
 
 #ifdef HZ_PLATFORM_WINDOWS
 	#include <Windows.h>
